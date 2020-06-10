768802ddee63c6dac35572107c35b1a30b0bdb2c
Yolanda Robla <yroblamo@redhat.com>
2020-06-10 09:56:36 +0000

    Add new script to fix sriov driver in unsupported nics

---
+++ local_fixes/fix_ixgbe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this is needed to add a new driver to use sriov in unsupported cards
+SERVER=$1
+
+echo "Fixing driver in $SERVER"
+scp ./ixgbe-5.7.1-1.x86_64.rpm core@${SERVER}:/tmp/
+
+ssh core@${SERVER} <<EOF
+cd /tmp/
+rpm2cpio ./ixgbe-5.7.1-1.x86_64.rpm | cpio -idmv
+
+sudo ostree admin unlock --hotfix
+sudo cp lib/modules/4.18.0-147.8.1.el8_1.x86_64/updates/drivers/net/ethernet/intel/ixgbe/ixgbe.ko /usr/lib/modules/4.18.0-147.8.1.el8_1.x86_64/kernel/drivers/net/ethernet/intel/ixgbe/ixgbe.ko.xz
+sudo rmmod ixgbe
+sudo modprobe ixgbe
+EOF
+
